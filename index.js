f8f86c22054dc4800b3b1c17d460d2042e63ebd3
evaboogaard <evaboogaard@hotmail.com>
2022-03-02 16:23:05 +0100

    templating

--- index.js
+++ index.js
@@ -1,27 +1,25 @@
-const express = require('express')
+const express = require("express")
+const { engine } = require("express-handlebars")
 const app = express()
 
-const handlebars = require("handlebars");
+// const path = require('path');
 
-const PORT = process.env.PORT || 1337
+app.engine('.hbs', engine({
+  extname: '.hbs',
+  defaultLayout: 'main'
+}));
 
-app.use('/static', express.static('static'))
+app.set('view engine', '.hbs');
+app.set("views", "./views");
 
-app.get('/', onHome)
-app.get('/about', onAbout)
+app.get('/', (req, res) => {
+  res.render('home');
+});
 
-app.get('*', error)
+//error handling
+app.use((req, res) => {
+    res.status(404).send("Sorry, this page doesn't exist!")
+  })
 
-function onHome(req, res){
-    res.send('<h1>Hello Client</h1>')
-};
-
-function onAbout(req, res){
-    res.send('<h1>About Me</h1>')
-}
-
-function error(req, res){
-    res.send('error 404 not found', 404)
-}
-
+//port
 app.listen(3000)
