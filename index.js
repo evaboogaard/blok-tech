1b095ae02dc828cf6f7ddff984cd67ab09575f48
evaboogaard <evaboogaard@hotmail.com>
2022-03-08 21:08:28 +0100

    nothing works

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const path = require("path");
 // const slug = require("slug");
 const bodyParser = require("body-parser");
 const router = express.Router();
-const port = process.env.PORT; //|| 1337;
+const port = process.env.PORT || 1337;
 
 const mongoose = require("mongoose");
 
